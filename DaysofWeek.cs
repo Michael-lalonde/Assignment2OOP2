fee51f1fc6451f5eebb787b5c0a76ae08b6ed818
Mikethestrike23 <michael.lalonde1@edu.sait.ca>
2025-03-13 18:20:16 -0600

    add daysofWeek.cs

---
+++ DaysofWeek.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment2OOP2
+{
+    public class DaysOfWeek
+    {
+        public List<string> Days { get; private set; }
+
+        public DaysOfWeek()
+        {
+            Days = new List<string>
+            {
+                "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+            };
+        }
+
+        public void PrintDays()
+        {
+            foreach (var day in Days)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
